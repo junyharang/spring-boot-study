bbae31fd4e1df0f63c73bab914a39eaf24f44b86
홍세준 <juny@hongsejun-ui-MacBookAir.local>
2021-08-04 17:14:52 +0900

    Feat : 210804 - API에 대하여 공부

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,37 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
-    }
+    } // helloMvc() 끝
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    } // helloString() 끝
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+       Hello hello = new Hello();
+       hello.setName(name);
+
+       return hello;
+    } // helloAPI() 끝
+
+    static class Hello {
+        private String name;
+
+
+        // Getter와 Setter를 이용한 자바 Bean 방식은 property 접근 방식
+        public String getName() {
+            return name;
+        } // getName()끝
+
+        public void setName(String name) {
+            this.name = name;
+        } // setName() 끝
+
+    } // Hello() 끝
+
+
 } // Class 끝
